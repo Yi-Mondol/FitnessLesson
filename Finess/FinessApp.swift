44a9907392b98983faee918eb053aa706d8262b4
Mondol Yi <yimondol22@gmail.com>
2026-03-04 21:06:45 +0900

    Initial Commit

---
+++ Finess/FinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinessApp.swift
+//  Finess
+//
+//  Created by Yi Mondol on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinessTests/FinessTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinessTests.swift
+//  FinessTests
+//
+//  Created by Yi Mondol on 3/4/26.
+//
+
+import Testing
+@testable import Finess
+
+struct FinessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
